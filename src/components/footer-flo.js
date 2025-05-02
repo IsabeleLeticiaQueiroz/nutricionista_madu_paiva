53605c9989b4efd00f8cc09cbfd70fda6f466dd6
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-05-01 22:00:13 -0300

    Atualização links

--- src/components/footer-flo.js
+++ src/components/footer-flo.js
@@ -6,8 +6,8 @@ const FooterFlo = () => {
             <div className="container-footer" id='footer'>
                 <div className="row-footer">
                     {/* <img src="/imgs/mdi_linkedin.svg" alt="" /> */}
-                    <img src="/imgs/ri_whatsapp-fill.svg" alt="" />
-                    <img src="/imgs/insta.svg" alt="" />
+                    <a href="https://wa.me/message/U7KX6BPRBVOYK1"><img src="/imgs/ri_whatsapp-fill.svg" alt="" /></a>
+                    <a href="https://www.instagram.com/madupaivanutriprofissional/"><img src="/imgs/insta.svg" alt="" /></a>
                 </div>
             </div>
         </footer>
